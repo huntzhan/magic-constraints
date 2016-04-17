4aa685397bd89f590234321395c800361711dc2c
huntzhan <programmer.zhx@gmail.com>
2016-04-17 18:56:29 +0800

    Fixed setup.py.

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 from __future__ import (
     division, absolute_import, print_function, unicode_literals,
 )
-from builtins import *                  # noqa
-from future.builtins.disabled import *  # noqa
 
 import codecs
 import imp
