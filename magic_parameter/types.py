4db43d44359654aaa446ae82a85f9d42b8bc1423
huntzhan <programmer.zhx@gmail.com>
2016-04-19 20:16:21 +0800

    Added demo for #6.

---
+++ magic_parameter/types.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals,
+)
+from builtins import *                  # noqa
+from future.builtins.disabled import *  # noqa
+from future.utils import with_metaclass
+
+from abc import ABCMeta
+import collections.abc as abc
+
+
+class MagicTypeGenerator(type):
+
+    def __new__(cls, ABC):
+
+        class GetItemHack(ABCMeta):
+
+            __getitem__ = cls.__getitem__
+            __instancecheck__ = cls.__instancecheck__
+
+        class MagicType(with_metaclass(GetItemHack, object)):
+
+            nested_type = None
+
+        # TODO: fix later.
+        MagicType.register(ABC)
+
+        return MagicType
+
+    # cls bound to real Magic type.
+    def __getitem__(cls, type_decl):
+        raise NotImplemented
+
+    # cls bound to real Magic type.
+    def __instancecheck__(cls, ins):
+        raise NotImplemented
+
+
+class SequenceGenerator(MagicTypeGenerator):
+
+    def __getitem__(cls, type_decl):
+
+        NestedSequence = SequenceGenerator(cls)
+        NestedSequence.nested_type = type_decl
+        return NestedSequence
+
+    def __instancecheck__(cls, ins):
+        if not cls.__subclasscheck__(type(ins)):
+            return False
+        # is sequence.
+        if cls.nested_type:
+            for e in ins:
+                print(e)
+                if not isinstance(e, cls.nested_type):
+                    return False
+        return True
+
+
+Sequence = SequenceGenerator(abc.Sequence)
---
+++ tests/test_types.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals,
+)
+from builtins import *                  # noqa
+from future.builtins.disabled import *  # noqa
+
+from magic_parameter.types import *  # noqa
+
+
+def test_shit():
+
+    assert isinstance([], Sequence)
+    assert isinstance([1, 2, 3], Sequence[int])
+    assert not isinstance([1, 2.0, 3], Sequence[int])
