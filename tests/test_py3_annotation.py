86a3b64b90b2b7af8f5c8fb90bf334314b556622
huntzhan <programmer.zhx@gmail.com>
2016-04-24 23:58:37 +0800

    Close #2.

--- tests/test_py3_annotation.py
+++ tests/test_py3_annotation.py
@@ -5,18 +5,11 @@ from __future__ import (
 from builtins import *                  # noqa
 from future.builtins.disabled import *  # noqa
 
-import sys
 import pytest
 from magic_constraints import *  # noqa
 
 
-pytestmark = pytest.mark.skipif(
-    sys.version_info.major < 3,
-    reason="",
-)
-
-
-def test_annotation():
+def test_function_annotation():
 
     @function_constraints
     def func1(foo: int, bar: float) -> float:
@@ -36,3 +29,28 @@ def test_annotation():
     assert 3.0 == func2(1, 2.0)
     with pytest.raises(TypeError):
         func2(1)
+
+
+def test_method_annotation():
+
+    class Example(object):
+
+        @classmethod
+        @method_constraints
+        def func1(cls, foo: int, bar: float) -> float:
+            return foo + bar
+
+        @method_constraints
+        def func2(self, foo: int, bar: float = None) -> float:
+            if bar is None:
+                # should fail the return type checkin.
+                return 42
+            else:
+                # good case.
+                return foo + bar
+
+    assert 3.0 == Example.func1(1, 2.0)
+    e = Example()
+    assert 3.0 == e.func2(1, 2.0)
+    with pytest.raises(TypeError):
+        e.func2(1)
